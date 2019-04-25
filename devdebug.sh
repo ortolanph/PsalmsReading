5d2878deb8e3f979b364b4aa7204b94707456042
Paulo Henrique Ortolan <paulo.ortolan@gmail.com>
2019-04-25 23:08:53 +0100

    Updating project files

--- devdebug.sh
+++ devdebug.sh
@@ -4,5 +4,5 @@ source environments/development.conf
 
 java -Dserver.port=$PORT \
      -Dspring.profiles.active=$PROFILE \
-      -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=$DEBUG_PORT \
+     -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=$DEBUG_PORT \
      -jar $JAR_LOCATION/$JAR_FILE
